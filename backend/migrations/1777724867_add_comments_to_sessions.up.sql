3f6d0b0e9ee97eb64affae2bfaacd3adb27caaa8
Nathan Ngo <nathanj238@gmail.com>
2026-05-04 09:15:01 +1000

    Added SQL linting and formatting

--- backend/migrations/1777724867_add_comments_to_sessions.up.sql
+++ backend/migrations/1777724867_add_comments_to_sessions.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    session_id INTEGER NOT NULL REFERENCES sessions(id) ON DELETE CASCADE,
-    text TEXT not NULL,
+    session_id INTEGER NOT NULL REFERENCES sessions (id) ON DELETE CASCADE,
+    text_content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
