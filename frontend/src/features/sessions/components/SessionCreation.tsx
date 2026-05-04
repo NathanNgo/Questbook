b6543f21e584badc81112f5cb63b9826e80df8f3
Nathan Ngo <nathanj238@gmail.com>
2026-05-04 10:39:15 +1000

    Remove failed test

--- frontend/src/features/sessions/components/SessionCreation.tsx
+++ frontend/src/features/sessions/components/SessionCreation.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { useCreateSessionMutation } from "../api/mutations";
 
-const FOO = "FOO"
-
 export function SessionCreation() {
     // Controlled State for Input
     const [sessionName, setSessionName] = useState("");
