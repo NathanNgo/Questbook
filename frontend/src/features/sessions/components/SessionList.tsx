a1b2b9b5213550011f693d53458ccc53577d2273
Mustafa Barodawala <24886321+itsjustmustafa@users.noreply.github.com>
2026-05-08 11:13:06 +1000

    Update SessionList.tsx
    Co-authored-by: Nathan Ngo <nathanj238@gmail.com>

--- frontend/src/features/sessions/components/SessionList.tsx
+++ frontend/src/features/sessions/components/SessionList.tsx
@@ -3,7 +3,7 @@ import type { Session } from "../api/types";
 import styles from "./SessionList.module.css";
 import { SessionListItem } from "./SessionListItem";
 
-interface SessionListProp {
+interface SessionListProps {
     sessions: Session[];
     onDeleteSession: (session: Session) => void;
     onChangeSessionName: (sessionId: number, newSessionName: string) => void;
