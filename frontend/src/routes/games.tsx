437a9ab3a6748462abaed166a970b78e22c28801
= <24886321+itsjustmustafa@users.noreply.github.com>
2026-05-16 22:28:04 +1000

    update imports for feature name session to games

--- frontend/src/routes/games.tsx
+++ frontend/src/routes/games.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { gamesQueryOptions } from "#/features/sessions/api/queries";
-import { GameCreation } from "#/features/sessions/components/GameCreation";
-import { GameList } from "#/features/sessions/components/GameListContainer";
+import { gamesQueryOptions } from "#/features/games/api/queries";
+import { GameCreation } from "#/features/games/components/GameCreation";
+import { GameList } from "#/features/games/components/GameListContainer";
 
 export const Route = createFileRoute("/games")({
     component: Games,
